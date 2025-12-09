1a34061732f11a106034b470f60829c01aea3872
jprajzne <dr3dwerkz@gmail.com>
2025-12-09 17:58:52 +0100

    UPD: Create flatpak for the application #14

--- uninstall.sh
+++ uninstall.sh
@@ -5,6 +5,7 @@ set -e
 
 APP_DIR="$HOME/.local/share/audio-recorder"
 DESKTOP_FILE="$HOME/.local/share/applications/org.gnome.AudioRecorder.desktop"
+ICON_FILE="$HOME/.local/share/icons/hicolor/scalable/apps/org.gnome.AudioRecorder.svg"
 
 echo "Uninstalling Audio Recorder..."
 
@@ -14,8 +15,14 @@ rm -rf "$APP_DIR"
 # Remove desktop file
 rm -f "$DESKTOP_FILE"
 
+# Remove icon
+rm -f "$ICON_FILE"
+
 # Update desktop database
 update-desktop-database "$HOME/.local/share/applications" 2>/dev/null || true
+
+# Update icon cache
+gtk-update-icon-cache "$HOME/.local/share/icons/hicolor" 2>/dev/null || true
 
 echo "Uninstallation complete!"
 
